5db663fbb3409832696c0e8e29b99a3fc5299f72
num22yh <113687179+num22yh@users.noreply.github.com>
2025-03-05 20:55:57 +0900

    Time: 1126 ms (5.09%), Space: 46 MB (9.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,21 +2,36 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+
+    static List<Integer> ans = new ArrayList<>();
+    static boolean flag = false;
+
     public int[] twoSum(int[] nums, int target) {
-        // nums 에서 조합해볼 수 있는 모든 2 가지 숫자 탐색 (O(N^2), 최대 10^8)
-        // 조합한 두 숫자의 합 == target 이면 그 수의 인덱스 반환
+        ans.clear(); // 리스트 초기화
+        flag = false; // flag 초기화
+        backtracking(0,nums, target);
+        return new int[]{ans.get(0),ans.get(1)};
+    }
 
-        int[] result = new int[2];
-
-        for(int i=0; i <nums.length-1; i++){
-            for(int j=i+1; j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    result[0] = i;
-                    result[1] = j;
-                }
+    public static void backtracking(int start, int[] nums, int target){
+       
+        if(ans.size() == 2){
+            if(nums[ans.get(0)]+nums[ans.get(1)] == target){
+                flag = true;
             }
+
+            return;
         }
 
-        return result;
+        for(int i= start; i<nums.length; i++){
+            ans.add(i);
+            backtracking(i+1, nums, target);
+            if(flag){
+                return;
+            }
+            ans.remove(ans.size() - 1);
+            
+        }
     }
+
 }
