76e4d8b3ecab9e0e7f31fcc9a5133a233a876137
num22yh <113687179+num22yh@users.noreply.github.com>
2026-03-28 19:18:53 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0046-permutations/0046-permutations-03-28-2026-19-19-08.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        
+        boolean[] visited = new boolean[nums.length];
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtracking(new ArrayList<>(), nums, visited , result );
+        return result;
+    }
+
+    private void backtracking(List<Integer> answer, int[] nums , boolean[] visited , List<List<Integer>> result){
+
+        if(answer.size() == nums.length){
+    
+            result.add(new ArrayList<>(answer));
+            
+            return;
+        }
+
+        for(int i=0; i<nums.length; i++){
+
+            if(!visited[i]){
+                
+                visited[i] = true;
+                answer.add(nums[i]);
+                backtracking(answer, nums, visited, result);
+                answer.remove(answer.size()-1);
+                visited[i] = false;
+
+            }
+        
+        }
+
+
+    }
+}
