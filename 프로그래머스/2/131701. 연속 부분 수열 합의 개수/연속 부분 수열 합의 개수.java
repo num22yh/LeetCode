a82c0df2a44d5e8166582c57fbf99a296f40d81a
num22yh <113687179+num22yh@users.noreply.github.com>
2026-05-03 22:50:34 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 179.18 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> sumSet = new HashSet<>();
+        
+        int answer = 0;
+    
+        for (int i = 0;  i < elements.length; i++ ) {
+            sumSet.add(elements[i]);
+            dfs(elements, 0, i, 0, sumSet);
+        }
+        return sumSet.size();
+    }
+    
+    private void dfs(int[] elements, int level, int cur, int sum, Set<Integer> sumSet ){
+        
+        if(level == elements.length){
+            return;
+        }
+        
+        int next;
+        if(cur + 1 < elements.length){
+             next = cur + 1;
+        }else{
+            next = 0;
+        }
+        
+        sum = sum + elements[next];
+        sumSet.add(sum);
+        
+        dfs(elements, level+1, next, sum, sumSet);
+        
+        return;
+    }
+}
